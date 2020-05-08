727d1eb53df3c71f939dcee91ede8221b7b2c596
Vlad <greensmoke71@gmail.com>
2020-05-08 17:40:26 +0300

    adaptive header + promo

--- src/js/main.js
+++ src/js/main.js
@@ -54,7 +54,26 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+
+
+  const menu = document.querySelector('.header-navigation__list'),
+      menuItem = document.querySelectorAll('.header-navigation__list-item'),
+      hamburger = document.querySelector('.header__burger-button');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('header__burger-button--active');
+    menu.classList.toggle('header-navigation__list--active');
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('header__burger-button--active');
+      menu.classList.toggle('header-navigation__list--active');
+    })
+  })
+
 });
+
 
 
 
